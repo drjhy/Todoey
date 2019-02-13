42537f5011ddf360d993285705de3810d244839f
John Young <johnyoung@johns-macbook-pro.local>
2019-02-13 07:24:00 -0500

    Persisting data using Core Data.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import CoreData
+
 
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     
-   let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("items.plist")
+    var selectedCategory : Category? {
+        
+        didSet{
+            
+            loadItems()
+        }
+    }
+    
+    let context = (UIApplication.shared.delegate as!  AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-     loadItems()
-        
         
     }
 
@@ -68,11 +74,15 @@ class TodoListViewController: UITableViewController {
         
         let  action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
+            
             //what will happen once the  user clicks  the Add Item button on our  UIAert
             
-            let  newItem  = Item()
-            newItem.title = textField.text!
             
+            
+            let  newItem  = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             
             self.saveItems()
@@ -93,15 +103,13 @@ class TodoListViewController: UITableViewController {
     
     func saveItems() {
         
-        let encoder = PropertyListEncoder()
-        
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+         
+            try context.save()
             
         } catch {
             
-            print("Error encoding item array, \(error)")
+         print("Error saving context \(error)")
             
         }
         
@@ -111,24 +119,62 @@ class TodoListViewController: UITableViewController {
         
     }
    
-    func loadItems() {
-    
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            
-            let decoder =  PropertyListDecoder()
-            
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-            
-        } catch {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
         
-        print("Error decoding item array,n \(error)")
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let addtionalPredicate = predicate {
             
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate,addtionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
         }
-    
+        
+        do {
+        itemArray = try context.fetch(request)
+        } catch {
+            
+            print("Error fetching data from context \(error)")
+        }
+        
+        tableView.reloadData()
     }
-    
-    
-}
 
 }
+
+
+
+// MARK -- Search bar methods
+
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+        
+        }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            // loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+    
+}
+    
+    
+    
+
+
+
+
